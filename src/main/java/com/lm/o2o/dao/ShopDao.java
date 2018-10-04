3043fc7ce51a0d2d42d1e135760e9eb6013271b8
Luo Man <811789670@qq.com>
2018-10-04 09:06:13 +0800

    finish updateShop

--- src/main/java/com/lm/o2o/dao/ShopDao.java
+++ src/main/java/com/lm/o2o/dao/ShopDao.java
@@ -2,6 +2,9 @@ package com.lm.o2o.dao;
 
 import com.lm.o2o.entity.Shop;
 
+/**
+ * @author LuoMan
+ */
 public interface ShopDao {
     /**
      * 用于插入商铺数据
@@ -9,4 +12,11 @@ public interface ShopDao {
      * @return 插入的条数
      */
 	int insertShop(Shop shop);
+
+    /**
+     * 更新店铺信息
+     * @param shop shop实体
+     * @return 插入的条数
+     */
+	int updateShop(Shop shop);
 }
